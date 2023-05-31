a9f882ab61f5ab518583d84cec5849d7a7af0049
BaHaMuT-J <siranat.j@gmail.com>
2023-05-31 14:59:21 +0700

    first fix and test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    printf("hello world\n");
+    printf("check\n");
+    //check
+    // /home/bahamut/PCSA/test.c
+    for(int i=0;i<10;i++){
+        printf("%d ", i);
+    }
+    printf("\n");
+    printf("is");
+}
